6396f245d87967db66cc4f2104dc394b6a441fc2
brentluna <brentadamluna@gmail.com>
2016-10-05 09:19:25 -0700

    added topoloical sort

---
+++ topological sort/lib/graph.rb
@@ -0,0 +1,18 @@
+class Vertex
+	attr_accessor :value, :in_edges, :out_edges
+  def initialize(value)
+  	@value = value 
+  	@in_edges = []
+  	@out_edges = []
+  end
+end
+
+class Edge
+  def initialize(from_vertex, to_vertex, cost = 1)
+
+  end
+
+  def destroy!
+
+  end
+end
